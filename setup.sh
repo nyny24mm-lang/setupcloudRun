d4c4a4d4800d1877315fdf48fef0198d26f64a3d
nyny24mm-lang <nyny24.mm@gmail.com>
2025-09-29 22:45:21 +0630

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -109,9 +109,9 @@ case "$PROTO" in
 esac
 
 # ===== Telegram push =====
-BOT_TOKEN="7996106285:AAEvwouHVXXbjexPoxXcGnQqS4NBhhhQRnU"
-CHAT_ID="5608710234"
-MESSAGE="${URI}"
+BOT_TOKEN="${BOT_TOKEN:-}"
+CHAT_ID="${CHAT_ID:-}"
+MESSAGE="${HOST}"
 
 if [[ -n "${BOT_TOKEN}" && -n "${CHAT_ID}" ]]; then
   curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
